0bf6fa9dddfb7e873eeba40981270f6ae4743b51
bryanbill <emobrya@gmail.com>
2021-12-06 22:52:35 +0300

    created all the necessary entities

--- src/app/entities/index.ts
+++ src/app/entities/index.ts
@@ -1,1 +1,4 @@
 export { User } from './user.entity';
+export { Project } from './project.entity';
+export { Issue } from './issue.entity';
+export { Comment } from './comment.entity';
--- src/app/entities/user.entity.ts
+++ src/app/entities/user.entity.ts
@@ -1,20 +1,54 @@
-// import { hashPassword } from '@foal/core';
-import { BaseEntity, /*Column, */Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  /*Column, */ Entity,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  RelationId,
+  UpdateDateColumn,
+} from "typeorm";
+import { Issue, Project, Comment } from ".";
+import is from "../helpers/utils/validation";
 
-@Entity()
+@Entity({
+  name: "users",
+})
 export class User extends BaseEntity {
+  static validations = {
+    name: [is.required(), is.maxLength(100)],
+    email: [is.required(), is.email(), is.maxLength(200)],
+  };
 
   @PrimaryGeneratedColumn()
   id: number;
 
-  // @Column({ unique: true })
-  // email: string;
+  @Column("varchar")
+  name: string;
 
-  // @Column()
-  // password: string;
+  @Column("varchar")
+  email: string;
 
-  // async setPassword(password: string) {
-  //   this.password = await hashPassword(password);
-  // }
+  @Column("varchar", { length: 2000 })
+  avatarUrl: string;
 
+  @CreateDateColumn({ type: "timestamp" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  updatedAt: Date;
+
+  @OneToMany(() => Comment, (comment) => comment.user)
+  comments: Comment[];
+
+  @ManyToMany(() => Issue, (issue) => issue.users)
+  issues: Issue[];
+
+  @ManyToOne(() => Project, (project) => project.users)
+  project: Project;
+
+  @RelationId((user: User) => user.project)
+  projectId: number;
 }
