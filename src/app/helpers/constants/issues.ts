83eafcbf2bea39e578af7af033316759f09d74f3
bryanbill <emobrya@gmail.com>
2021-12-06 22:28:58 +0300

    issues and project constants

---
+++ src/app/helpers/constants/issues.ts
@@ -0,0 +1,25 @@
+export enum IssueType {
+  Bug = "Bug",
+  Feature = "Feature",
+  Task = "Task",
+  STORY = "STORY",
+  EPIC = "EPIC",
+  OTHER = "OTHER",
+}
+
+export enum IssueStatus {
+  BACKLOG = "BACKLOG",
+  INPROGRESS = "IN_PROGRESS",
+  DONE = "DONE",
+  REJECTED = "REJECTED",
+  CANCELED = "CANCELED",
+  SELECTED = "SELECTED",
+}
+
+export enum IssuePriority {
+  CRITICAL = "5",
+  HIGH = "4",
+  MEDIUM = "3",
+  LOW = "2",
+  NONE = "1",
+}
